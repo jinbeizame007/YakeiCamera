99423c120b3a1ba0825674fcc83d84c8d951e58c
Momendohu <pinog.jh@gmail.com>
2018-07-11 16:43:42 +0900

    ウェブカメラテスト

---
+++ Assets/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour {
+    //=============================================================
+    public WebCamTexture webCameraTexture = null;
+    public GameObject plane;
+
+    //=============================================================
+    private void Init(){
+		CRef();
+	}
+
+	//=============================================================
+	private void CRef(){
+		
+	}
+
+	//=============================================================
+	private void Awake () {
+		Init();
+	}
+
+	private void Start () {
+        webCameraTexture = new WebCamTexture();
+        plane.GetComponent<Renderer>().material.mainTexture = webCameraTexture;
+        webCameraTexture.Play();
+    }
+	
+	private void Update () {
+		
+	}
+}
